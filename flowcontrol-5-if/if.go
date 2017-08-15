dc78997174c463f5ace44aeb91c9ed0ba97ba6b8
kmsheng <kmsh3ng@gmail.com>
2017-08-16 00:52:02 +0800

    add if else example

---
+++ flowcontrol-5-if/if.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    brokenHeart := true
+    if brokenHeart {
+        fmt.Println("Don't worry too much")
+    } else {
+        fmt.Println("Good for you")
+    }
+}
