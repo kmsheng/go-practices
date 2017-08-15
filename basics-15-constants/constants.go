ebe562663fde979d6642af53d2d2b3b166d3b6d1
kmsheng <kmsh3ng@gmail.com>
2017-08-16 00:18:06 +0800

    new file constants.go

---
+++ basics-15-constants/constants.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+func main() {
+    const World = "world"
+    fmt.Println("Hello", World)
+    fmt.Println("Happy", Pi, "Day")
+
+    const Truth = true
+    fmt.Println("Go rules?", Truth);
+    blah := 1
+    fmt.Println(blah)
+
+}
