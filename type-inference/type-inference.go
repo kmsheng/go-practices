61746de6ff90efc41f2cb97d676f125c627dd161
kmsheng <kmsh3ng@gmail.com>
2017-08-16 00:13:48 +0800

    new file type-inference.go

---
+++ type-inference/type-inference.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    num := 1
+    floatNum := 0.2
+    str := "str"
+    imaginaryNum := 0.5i
+    fmt.Printf("num is of type %T\n", num)
+    fmt.Printf("floatNum is of type %T\n", floatNum)
+    fmt.Printf("str is of type %T\n", str)
+    fmt.Printf("imaginaryNum is of type %T\n", imaginaryNum)
+}
