beab091d8a358a7d9bb1a935e4d4ed79984f6b26
kmsheng <kmsh3ng@gmail.com>
2017-08-16 15:26:10 +0800

    update example of struct

--- moretypes-2-structs/structs.go
+++ moretypes-2-structs/structs.go
@@ -2,12 +2,21 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-    X int
-    Y int
+type Cube struct {
+    length int
+}
+
+func (c Cube) volume() int {
+    return c.length * c.length * c.length
+}
+
+func (c Cube) surfaceArea() int {
+    return c.length * c.length * 6
 }
 
 func main() {
-    fmt.Println(Vertex{1, 2})
-    fmt.Println(Vertex{3, 4})
+    c1 := Cube{3}
+    fmt.Println("Cube", c1)
+    fmt.Println("Cube volume is: ", c1.volume())
+    fmt.Println("Cube surface area is: ", c1.surfaceArea())
 }
