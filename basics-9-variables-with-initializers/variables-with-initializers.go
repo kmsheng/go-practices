b91d54bfc90183064a43531dce99186ca1003c1b
kmsheng <kmsh3ng@gmail.com>
2017-08-31 13:39:13 +0800

    add variable initializers example

---
+++ basics-9-variables-with-initializers/variables-with-initializers.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var bool1, bool2 bool = false, false
+var int1, int2 int = 1, 1
+
+func main() {
+    fmt.Printf("bool1 = %t\nbool2 = %t\nint1 = %d\nint2 = %d\n", bool1, bool2, int1, int2)
+}
