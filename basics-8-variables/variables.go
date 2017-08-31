395fee3464cd657b792a69606a8813a521a15901
kmsheng <kmsh3ng@gmail.com>
2017-08-31 13:33:22 +0800

    add variables example

---
+++ basics-8-variables/variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var first, second, third bool
+
+func main() {
+    var anotherVariable int
+    // false false false 0
+    fmt.Println(first, second, third, anotherVariable)
+}
