1c8847ed6d0844e60c754c5903b80e65390f05b8
kmsheng <kmsh3ng@gmail.com>
2017-09-03 20:26:33 +0800

    update example of numeric constants

--- basics-16-numeric-constants/numeric-constants.go
+++ basics-16-numeric-constants/numeric-constants.go
@@ -1,25 +1,27 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math"
+)
 
 const (
     Big = 1 << 100
-    Small = Big >> 99
 )
 
 func needInt(x int) int {
-    return x * 10 + 1
+    return x + 1
 }
 
+// An untyped constant takes the type needed by its context.
 func needFloat(x float64) float64 {
     return x * 0.1
 }
 
 func main() {
-    fmt.Println(needInt(Small))
-    fmt.Println(needFloat(Small))
-    fmt.Println(needFloat(Big))
+    fmt.Printf("The maximun value for float64 is %v\n", math.MaxFloat64)
+    fmt.Printf("Type of needFloat(Big): %T\n", needFloat(Big))
 
-    // this line will cause overflow error
-    // fmt.Println(needInt(Big))
+    // overflows int
+    // fmt.Printf("Type of needInt(Big): %T\n", needInt(Big))
 }
